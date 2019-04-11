fbcf797825d3660a1f0cb26f86f86ea9c78aabe3
vlbos <lisheng2007@hotmail.com>
2019-04-11 13:49:00 +0800

    bos.oracle

--- build.sh
+++ build.sh
@@ -8,6 +8,6 @@ NC='\033[0m'
 CORES=`getconf _NPROCESSORS_ONLN`
 mkdir -p build
 pushd build &> /dev/null
-cmake ../
+cmake -DEOSIO_ROOT=/Users/lisheng/opt/eosio -DCMAKE_MODULE_PATH=/Users/lisheng/opt/eosio/lib/cmake/eosio ../
 make -j${CORES}
 popd &> /dev/null
