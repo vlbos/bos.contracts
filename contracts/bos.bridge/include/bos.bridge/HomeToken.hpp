5eff7a7aecb56a1b3e34379d8aeebf821cd7e5fb
vlbos <vlbos2018@gmail.com>
2020-01-10 16:07:28 +0800

    bridge

---
+++ contracts/bos.bridge/include/bos.bridge/HomeToken.hpp
@@ -0,0 +1,16 @@
+#include <eosio/eosio.hpp>
+#include "bos.bridge/bos.config.hpp"
+#include "bos.bridge/interfaces/IBridgeValidators.hpp"
+#include "bos.bridge/interfaces/IBurnableMintableToken.hpp"
+
+class HomeToken : public IBurnableMintableToken{
+
+   bool mint(name _to, uint64_t _amount) 
+   {
+
+   }
+     void burn(uint64_t _value)  
+     {
+       
+     }
+}
